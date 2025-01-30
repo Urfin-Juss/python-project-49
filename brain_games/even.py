cce090ba3977a41c462a417047adfb427eeaf4f8
Urfin-Juss <sergeitch@gmail.com>
2025-01-30 09:15:12 +0700

    step 5 logic done

--- brain_games/even.py
+++ brain_games/even.py
@@ -1,21 +1,22 @@
 # check_even
-import random
 import prompt
+import random
 # from brain_games.cli import *
 
 
 def check_even(user_name):
     number = random.randint(1, 99)
-    even_answer = prompt.string(
-        f'Answer "yes" if the number is even, otherwise answer "no".\nQuestion: {number}\nAnswer: '
-    )
+    print(f'Answer "yes" if the number is even, otherwise answer "no".\nQuestion: {number}')
+    even_answer = prompt.string('Answer: ')
     if number % 2 == 0 and even_answer == "yes":
         print("Correct!")
+    elif number % 2 != 0 and even_answer == "no":
+        print("Correct!")
     else:
-        print(
-            f"'{even_answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {user_name}!"
-        )
-
+        even_status = "yes" if number % 2 == 0 else "no"
+        print(f"'{even_answer}' is wrong answer ;(. Correct answer was '{even_status}'.")
+        print(f"Let's try again, {user_name}!")
+        
     # if number % 2 != 0 and even_answer=='no':
     # else print(f"'{even_answer}' is ")
 
