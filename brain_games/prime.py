85747b78af499a11d675e5b214b74904abe73867
Urfin-Juss <sergeitch@gmail.com>
2025-04-03 23:35:47 +0700

    Refactoring3

--- brain_games/prime.py
+++ brain_games/prime.py
@@ -1,7 +1,9 @@
 # check_prime
+import math
+
 from brain_games.cli import welcome_user
 from brain_games.core import run_game
-from brain_games.utils import get_two_random_numbers, is_prime
+from brain_games.utils import get_two_random_numbers
 
 
 def check_prime():
@@ -19,3 +21,29 @@ def get_prime():
     correct_answer = "yes" if is_prime(number) else "no"
 
     return number, correct_answer
+
+
+# gen_progression
+def gen_progression(start, step, length):
+    return [start + step * i for i in range(length)]
+
+
+# valid_progression
+def is_valid_progression(progression):
+    return all(0 <= num <= 100 for num in progression)
+
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+
+    limit = int(math.sqrt(number)) + 1
+    for i in range(5, limit, 2):
+        if number % i == 0:
+            return False
+
+    return True
--- brain_games/utils.py
+++ brain_games/utils.py
@@ -1,6 +1,4 @@
 # utils.py
-
-import math
 import random
 
 import prompt
@@ -21,29 +19,3 @@ def ask_question(question: str, correct_answer: str) -> tuple[bool, str]:
     is_correct = user_answer == correct_answer.strip()
 
     return is_correct, user_answer
-
-
-# gen_progression
-def gen_progression(start, step, length):
-    return [start + step * i for i in range(length)]
-
-
-# valid_progression
-def is_valid_progression(progression):
-    return all(0 <= num <= 100 for num in progression)
-
-
-def is_prime(number):
-    if number <= 1:
-        return False
-    if number <= 3:
-        return True
-    if number % 2 == 0 or number % 3 == 0:
-        return False
-
-    limit = int(math.sqrt(number)) + 1
-    for i in range(5, limit, 2):
-        if number % i == 0:
-            return False
-
-    return True
